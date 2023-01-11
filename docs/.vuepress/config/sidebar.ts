cd6304344ec09ac805906b3f9ce153bacac4b7e4
SQLimit <110686480+SQLimit@users.noreply.github.com>
2023-01-11 14:26:19 +0800

    Update sidebar.ts

--- docs/.vuepress/config/sidebar.ts
+++ docs/.vuepress/config/sidebar.ts
@@ -148,6 +148,7 @@ export function getDocumentLv2Sidebar(
         path + "transparent_proxy/transparent_proxy.md",
         path + "tproxy.md",
         path + "tproxy_ipv4_and_ipv6.md",
+        path + "nginx_tls_tunnel.md",
         path + "iptables_gid.md",
         path + "redirect.md",
         path + "warp.md",
