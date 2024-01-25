61557298f8f64936fb0095e72f96d2e38ba9cdeb
Aoleou <124496859+aoleou@users.noreply.github.com>
2024-01-26 05:07:42 +0800

    Fix path error

--- docs/.vuepress/theme/clientAppEnhance.ts
+++ docs/.vuepress/theme/clientAppEnhance.ts
@@ -1,10 +1,10 @@
-import { defineClientAppEnhance } from "@vuepress/client";
-import Tab from "./components/Tab.vue";
-import Tabs from "./components/Tabs.vue";
-import Mermaid from "./components/Mermaid.vue";
+// import { defineClientAppEnhance } from "@vuepress/client";
+// import Tab from "./components/Tab.vue";
+// import Tabs from "./components/Tabs.vue";
+// import Mermaid from "./components/Mermaid.vue";
 
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  app.component("Tab", Tab);
-  app.component("Tabs", Tabs);
-  app.component("Mermaid", Mermaid);
-});
+// export default defineClientAppEnhance(({ app, router, siteData }) => {
+//   app.component("Tab", Tab);
+//   app.component("Tabs", Tabs);
+//   app.component("Mermaid", Mermaid);
+// });
