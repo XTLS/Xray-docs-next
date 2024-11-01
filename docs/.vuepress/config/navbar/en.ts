e3408bfee1fefc1ae35745f71137ab4e81a542b8
𐲓𐳛𐳪𐳂𐳐 𐲀𐳢𐳦𐳫𐳢 𐲥𐳔𐳛𐳪𐳌𐳑𐳖𐳇 <26771058+KobeArthurScofield@users.noreply.github.com>
2024-11-02 01:43:58 +0800

    Update Great Chronicles (#600)

--- docs/.vuepress/config/navbar/en.ts
+++ docs/.vuepress/config/navbar/en.ts
@@ -3,7 +3,7 @@ import { NavbarConfig } from '@vuepress/theme-default'
 // TODO: translation
 export const navbarEn: NavbarConfig = [
   { text: 'Homepage', link: '/en' },
-  { text: 'Website History', link: '/en/about/news.md' },
+  { text: 'The Great Chronicles', link: '/en/about/news.md' },
   { text: 'Config Reference', link: '/en/config/' },
   { text: 'Developer Guide', link: '/en/development/' },
   { text: 'Quick Start', link: '/en/document/' },
